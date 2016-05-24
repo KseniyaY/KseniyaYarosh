6327f4516547c63adaaff7555b7d712d3b3c7a2b
KseniyaY <okyarosh@ukr.net>
2016-05-24 16:56:24 +0300

    update all structure

---
+++ public/javascripts/events.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+// 	$("#backStage").hover(function(){
+// 		$(".theaterStage").css({'background': 'url(../images/theaterBackstage.jpg) center 0 repeat-x'})
+// 		}, function(){
+// 			$(".theaterStage").css({'background': 'url(../images/Romanian_Opera_House_Timisoara_cut.jpg) center 0 repeat-x'});
+// 	})
+// });
+
+
+//печатающийся текст
+
+//сохранить текст в переменную;
+//разбить на буквы, и создать массив, по которому пройтись перебором и при каждой итерации цикла выводить с интервалом каждую букву на экран
+
+
