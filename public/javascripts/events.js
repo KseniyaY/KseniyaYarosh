59dbe20dbd7c6538c65804cc0aaf1b0a468bf3ad
KseniyaY <okyarosh@ukr.net>
2016-06-08 00:56:41 +0300

    Update all files"

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,15 +1,137 @@
-// $(document).ready(function(){
-// 	$("#backStage").hover(function(){
-// 		$(".theaterStage").css({'background': 'url(../images/theaterBackstage.jpg) center 0 repeat-x'})
-// 		}, function(){
-// 			$(".theaterStage").css({'background': 'url(../images/Romanian_Opera_House_Timisoara_cut.jpg) center 0 repeat-x'});
-// 	})
-// });
+//изменение бекграунда при ховере на span как часть слогана
+$(document).ready(function(){
+	$("#backStage").hover(function(){
+	$(".theaterStage").css({'background': 'url(../images/theaterBackstage.jpg) center 0', 'background-size': 'cover'}),
+	$(".mainPart").css({'visibility': 'hidden'}),
+	$(".mainPart").text("Hello world!");
+	}, function(){
+	$(".theaterStage").css({'background': 'url(../images/Romanian_Opera_House_Timisoara_cut_2.jpg) center 0', 'background-size': 'cover'}),
+	$(".replacedPart").css({"display": "none"}),
+	$(".mainPart").text("Front-end is when you look at things this way!"),
+	$(".mainPart").css({'visibility': 'visible'});
+	});
+	//- $("#backStage").hover(function(){
+	//- $(".slogan").fadeOut();
+	//- }, function(){
+	//- $(".slogan").fadeIn();
+	//- });
+	});
 
 
-//печатающийся текст
+//набор приветствия при загрузке странички
+var text = "Welcome to my place!\nGlad to see you here!";
+i = 0;
+function type(){
+	i++;
+	if( i <= text.length )
+		document.querySelector(".typeText").innerHTML = text.substring(0, i);
+		setTimeout(type, 150);
+}
 
-//сохранить текст в переменную;
-//разбить на буквы, и создать массив, по которому пройтись перебором и при каждой итерации цикла выводить с интервалом каждую букву на экран
+type();
 
 
+
+//отправка сообщения из обратной формы на почтовый ящик
+$(function(){
+	// var visitorName = $("#visitorName").val();
+	// visitorEmail = $("#visitorEmail").val();
+	// visitorMessage = $("#visitorMessage").val();
+	// if(visitorName != "" || visitorEmail != "" || visitorMessage !="") {
+		sendMail();
+		respondOnSubmit();
+});
+
+function sendMail () {
+	var visitorName, visitorEmail, visitorMessage;
+			$("#sendButton").click(function() {
+				console.log('start function')
+				visitorName = $("#visitorName").val();
+				visitorEmail = $("#visitorEmail").val();
+				visitorMessage = $("#visitorMessage").val();
+				console.log('get data');
+				if(visitorName != "" && visitorEmail != "" && visitorMessage !="") {
+					$.post("/", {
+						visitorName: visitorName,
+						visitorEmail: visitorEmail,
+						visitorMessage: visitorMessage
+					})
+					console.log('post done')
+					$("textarea").val('');
+					return false; //чтобы не перезагружалась страничка, в качестве альтернативы preventDefault
+				}
+		});
+}
+
+
+function respondOnSubmit () {
+		$("#sendButton").click(function() {
+			 if ($("#visitorName").val() != '' && $("#visitorEmail").val() != '') {
+	    			//открыть модальное окно с id="afterSend"
+		   		 	$("#afterSend").modal('hide');
+		   		}
+		   	else {
+		   			$("#afterSend").modal('show');
+		   	}	
+	  	});
+}
+
+
+
+
+
+				//- var text = "Welcome to my place!\nGlad to see you here!";
+				//- i = 0;
+				//- function type(){
+				//- 	i++;
+				//- 	if( i <= text.length )
+				//- 		document.querySelector(".typeText").innerHTML = text.substring(0, i);
+
+				//- 	setTimeout(type, 150);
+				//- }
+
+				//- //- function disappear(){
+				//- //- 	document.querySelector(".typeText").style.visibility="hidden";
+				//- //- 	setTimeout(disappear, 12000);
+				//- //- }
+
+				
+				//- type();
+				//- disappear();
+
+				//- var text = "Dear guest, welcome to my site!";
+				//- i = 0;
+
+				//- function type(someString){
+				//- 	var someString = this;
+				//- 	i++;
+				//- 	if( i <= this.length )
+				//- 		document.querySelector(".typeText").innerHTML = this.substring(0, i);
+
+				//- 	setTimeout(type, 50);
+				//- }
+
+				//- type(text);
+				
+
+				//- $(function(){
+				//- 	sendMail();
+				//- });
+
+				//- function sendMail () {
+				//- 	var visitorName, visitorEmail, visitorMessage;
+				//- 	$("#sendButton").click(function() {
+				//- 		console.log('start function')
+				//- 		visitorName = $("#visitorName").val();
+				//- 		visitorEmail = $("#visitorEmail").val();
+				//- 		visitorMessage = $("#visitorMessage").val();
+				//- 		console.log('get data');
+				//- 		$.post("/", {
+				//- 			visitorName: visitorName,
+				//- 			visitorEmail: visitorEmail,
+				//- 			visitorMessage: visitorMessage
+				//- 		})
+				//- 		console.log('post done');
+				//- 		return false;
+				//- 	}) //чтобы не перезагружалась страничка, в качестве альтернативы preventDefault
+				//- }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var smtpConfig = require('../bin/config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome, my dear guest!' });
 });
 
-router.get('/page2', function(req, res, next) {
-  res.render('page2');
+// router.get('/some', function(req, res){
+// 	res.render('some');
+// });
+
+router.post('/', function(req, res) {
+	var visitorName = req.body.visitorName;
+	var visitorEmail = req.body.visitorEmail;
+	var visitorMessage = req.body.visitorMessage;
+	console.log(visitorEmail);
+	console.log(visitorName);
+
+
+	var transporter = nodemailer.createTransport(smtpConfig);
+	var mailOptions = {
+		from: {
+			name: visitorName,
+			address: visitorEmail
+		},
+		to: "developing.frontend@gmail.com", 
+		replyTo: visitorEmail,
+		subject: "This message from my site",
+		text: visitorMessage
+	}
+
+	transporter.sendMail(mailOptions, function(error, info) {
+		if(error) {
+			console.log("Error");
+			res.json({
+				blablabla: "Error"
+			})
+		}
+		else {
+			console.log("Message is sent: " + info.response)
+		}
+	});
 });
+
+// router.get('/page2', function(req, res, next) {
+//   res.render('page2');
+// });
 
 module.exports = router;
